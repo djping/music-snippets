e7c9284f96a4f9ead027d07e88383f158c178e71
djping <1938432787@qq.com>
2019-06-12 22:49:00 +0800

    歌曲组件

---
+++ song-list.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="song-list">
+    <div class="song-item" 
+      v-for="(song, index) in songs" 
+      :key="index"
+      @click="selectItem(song, index)"
+    >
+      <h1></h1>
+      <p></p>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    songs: {
+      type: Array,
+      default: null
+    }
+  },
+  methods: {
+    selectItem (song, index) {
+      this.$emit('select', index)
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
